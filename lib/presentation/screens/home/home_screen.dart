bd6886fa89ee6fa4ac5a4a15f6094e3e0122ef5c
Lnx <yermin.linos@ug.edu.ec>
2024-05-03 11:05:54 -0500

    MaterialPageRoute

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreem extends StatelessWidget {
   const HomeScreem({super.key});
@@ -53,7 +54,11 @@ class _CustomListTitle extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
       onTap: () {
-        //TODO: navegar a otras pantallas
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const ButtonsScreen(),
+          ),
+        );
       },
     );
   }
