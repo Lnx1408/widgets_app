98f64a453bcf2877353c29c9115269060902197b
Lnx <yermin.linos@ug.edu.ec>
2024-05-03 11:09:40 -0500

    pushNamed - routes

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreem extends StatelessWidget {
   const HomeScreem({super.key});
@@ -54,11 +53,14 @@ class _CustomListTitle extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
       onTap: () {
+        /*
         Navigator.of(context).push(
           MaterialPageRoute(
             builder: (context) => const ButtonsScreen(),
           ),
         );
+        */
+        Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
